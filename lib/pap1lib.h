93a60ee657b67eb1afc6ebe1a633e045c49cdbb7
jpro-y-del <j7juanp@gmail.com>
2025-05-07 16:33:14 -0300

    first commit

---
+++ lib/pap1lib.h
@@ -0,0 +1,11 @@
+#ifndef PAP1LIB_H
+#define PAP1LIB_H
+#include "arduino.h"
+void pasoNormal_init(int IN0, int IN1, int IN2, int IN3);
+void pasoNormal(int IN0, int IN1, int IN2, int IN3, int pasos);
+void pasoCompleto_init(int IN0, int IN1, int IN2, int IN3);
+void pasoCompleto(int IN0, int IN1, int IN2, int IN3, int pasos);
+void pasoMedio_init(int IN0, int IN1, int IN2, int IN3);
+void pasoMedio(int IN0, int IN1, int IN2, int IN3, int pasos);
+
+#endif 
