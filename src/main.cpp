0b31cfdaf06ed5d02ed657a34221b0caf789e849
jpro-y-del <j7juanp@gmail.com>
2025-05-07 18:49:56 -0300

    completo con punto3

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include "pap1lib.h"
+int i;
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pasoNormal_init (1,2,3,4);
+  pasoCompleto_init(5,6,7,8);
+  pasoMedio_init(9,10,11,12);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  pasoNormal(1,2,3,4,15);
+  pasoCompleto(5,6,7,8,12);
+  pasoMedio(9,10,11,12,36);
 }
